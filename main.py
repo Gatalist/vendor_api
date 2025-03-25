59a821c0d1ab5c31dd0a472ee7e3ded6fa48941d
Gatalist <kostencko.alexander2012@gmail.com>
2025-03-25 21:14:54 +0200

    second commit

---
+++ main.py
@@ -0,0 +1,55 @@
+# import requests
+# import xml.etree.ElementTree as ET
+#
+# url = "https://products.mti.ua/api/?action=loadContent&key=Sqceh4xB9PvL&cat_id=216"
+# response = requests.get(url)
+# root = ET.fromstring(response.content)
+#
+#
+# products_count = root.find("products_count").text
+# memory_used = root.find("memory_used").text
+#
+#
+#
+# print(f"Количество товаров: {products_count}")
+# print(f"Использовано памяти: {memory_used} MB")
+#
+# categories = []
+# for category in root.find("categorieslist").findall("category"):
+#     categories.append({
+#         "id": category.get("id"),
+#         "parentID": category.get("parentID"),
+#         "name": category.text
+#     })
+#
+# print(categories)
+#
+#
+# params = []
+# for param in root.find("paramslist").findall("param"):
+#     params.append({
+#         "type": param.get("type"),
+#         "code": param.get("code"),
+#         "name": param.get("name"),
+#         "in_filter": param.get("in_filter"),
+#         "multiple": param.get("multiple"),
+#         "sort": param.get("sort"),
+#     })
+#
+# print(params)
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "This is your API response"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
