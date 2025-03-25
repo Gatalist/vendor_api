3b322bfe6916b5ecedddd1efda1d7bbcd5dbe964
Gatalist <kostencko.alexander2012@gmail.com>
2025-03-26 01:04:34 +0200

    fix HOST

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,6 +3,7 @@ import datetime
 
 
 class Config(object):
+    HOST: str = "0.0.0.0"
     SECRET_KEY = os.getenv('SECRET_KEY')
     SESSION_LIFETIME = datetime.timedelta(days=10)
     ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
